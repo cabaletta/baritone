8837810e63c9156959e1cc009e2b05d4bf462d30
Leijurv <leijurv@gmail.com>
2018-08-01 23:30:12 -0400

    what

--- src/main/java/baritone/bot/pathing/goals/GoalComposite.java
+++ src/main/java/baritone/bot/pathing/goals/GoalComposite.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package baritone.bot.goals;
+package baritone.bot.pathing.goals;
 
 import java.util.Arrays;
 import java.util.Collection;
