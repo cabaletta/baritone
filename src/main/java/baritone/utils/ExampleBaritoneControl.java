b56dde8f7ec11761a3e9f63167e5cd8dd7d20f66
Leijurv <leijurv@gmail.com>
2018-08-23 13:17:29 -0700

    typo

--- src/main/java/baritone/utils/ExampleBaritoneControl.java
+++ src/main/java/baritone/utils/ExampleBaritoneControl.java
@@ -133,7 +133,7 @@ public class ExampleBaritoneControl extends Behavior {
         }
         if (msg.toLowerCase().startsWith("mine")) {
             String blockType = msg.toLowerCase().substring(4).trim();
-            List<BlockPos> locs = new ArrayList<>(WorldProvider.INSTANCE.getCurrentWorld().cache.getLocationsOf(1, 1));
+            List<BlockPos> locs = new ArrayList<>(WorldProvider.INSTANCE.getCurrentWorld().cache.getLocationsOf(blockType, 1, 1));
             if (locs.isEmpty()) {
                 displayChatMessageRaw("No locations known");
                 event.cancel();
