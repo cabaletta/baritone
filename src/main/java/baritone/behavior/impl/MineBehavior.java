f51bcc8b8919709139670f541d96918676eb7a32
Leijurv <leijurv@gmail.com>
2018-09-04 08:51:58 -0700

    forgot null check

--- src/main/java/baritone/behavior/impl/MineBehavior.java
+++ src/main/java/baritone/behavior/impl/MineBehavior.java
@@ -80,7 +80,7 @@ public class MineBehavior extends Behavior {
     }
 
     public void mine(String... mining) {
-        this.mining = new ArrayList<>(Arrays.asList(mining));
+        this.mining = mining == null ? null : new ArrayList<>(Arrays.asList(mining));
     }
 
     public void cancel() {
