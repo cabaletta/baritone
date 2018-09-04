bba2458f8eb8d51fb3d3301ffb06ebbf155419ba
Brady <zeromemesdev@gmail.com>
2018-09-04 18:48:00 -0500

    Separate "launch" into another SourceSet

--- src/launch/java/baritone/launch/mixins/MixinGuiScreen.java
+++ src/launch/java/baritone/launch/mixins/MixinGuiScreen.java
@@ -17,25 +17,32 @@
 
 package baritone.launch.mixins;
 
-import baritone.utils.accessor.IAnvilChunkLoader;
-import net.minecraft.world.chunk.storage.AnvilChunkLoader;
-import org.spongepowered.asm.mixin.Final;
+import baritone.Baritone;
+import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-
-import java.io.File;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * @author Brady
- * @since 9/4/2018
+ * @since 7/31/2018 10:38 PM
  */
-@Mixin(AnvilChunkLoader.class)
-public class MixinAnvilChunkLoader implements IAnvilChunkLoader {
+@Mixin(GuiScreen.class)
+public class MixinGuiScreen {
 
-    @Shadow @Final private File chunkSaveLocation;
-
-    @Override
-    public File getChunkSaveLocation() {
-        return this.chunkSaveLocation;
+    @Redirect(
+            method = {
+                    "isCtrlKeyDown",
+                    "isShiftKeyDown",
+                    "isAltKeyDown"
+            },
+            at = @At(
+                    value = "INVOKE",
+                    target = "org/lwjgl/input/Keyboard.isKeyDown(I)Z",
+                    remap = false
+            )
+    )
+    private static boolean isKeyDown(int keyCode) {
+        return Baritone.INSTANCE.getInputOverrideHandler().isKeyDown(keyCode);
     }
 }
