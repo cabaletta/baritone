29391af5ad76386f27037f8c98f902857d46f81a
Leijurv <leijurv@gmail.com>
2018-09-14 15:57:46 -0700

    solidarity

--- src/main/java/baritone/pathing/movement/movements/MovementParkour.java
+++ src/main/java/baritone/pathing/movement/movements/MovementParkour.java
@@ -191,13 +191,8 @@ public class MovementParkour extends Movement {
     @Override
     public MovementState updateState(MovementState state) {
         super.updateState(state);
-        switch (state.getStatus()) {
-            case WAITING:
-                state.setStatus(MovementState.MovementStatus.RUNNING);
-            case RUNNING:
-                break;
-            default:
-                return state;
+        if (state.getStatus() != MovementState.MovementStatus.RUNNING) {
+            return state;
         }
         if (dist >= 4) {
             state.setInput(InputOverrideHandler.Input.SPRINT, true);
