1b1670427b702929fd9ac5e8cfce55521ae9a19a
Leijurv <leijurv@gmail.com>
2018-09-25 08:06:39 -0700

    bump version and move proguard to scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 set -e # this makes the whole script fail immediately if any one of these commands fails
 ./gradlew build
-export VERSION=$(cat build.gradle| grep "version '" | cut -d "'" -f 2-2)
+export VERSION=$(cat build.gradle | grep "version '" | cut -d "'" -f 2-2)
 wget https://downloads.sourceforge.net/project/proguard/proguard/6.0/proguard6.0.3.zip
 unzip proguard6.0.3.zip 2>&1 > /dev/null
 cd build/libs
 echo "-injars 'baritone-$VERSION.jar'" >> api.pro # insert current version
-cat ../../proguard.pro | grep -v "this is the rt jar" | grep -v "\-injars" >> api.pro # remove default rt jar and injar lines
+cat ../../scripts/proguard.pro | grep -v "this is the rt jar" | grep -v "\-injars" >> api.pro # remove default rt jar and injar lines
 echo "-libraryjars '$(java -verbose 2>/dev/null | sed -ne '1 s/\[Opened \(.*\)\]/\1/p')'" >> api.pro # insert correct rt.jar location
 tail api.pro # debug, print out what the previous two commands generated
 cat api.pro | grep -v "\-keep class baritone.api" > standalone.pro # standalone doesn't keep baritone api
