cd9f7637b41330b8b8dd17e943e31ac60c801232
Leijurv <leijurv@gmail.com>
2018-09-25 12:27:24 -0700

    less verbose wget

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@ set -e # this makes the whole script fail immediately if any one of these comman
 ./gradlew build
 export VERSION=$(cat build.gradle | grep "version '" | cut -d "'" -f 2-2)
 
-wget https://downloads.sourceforge.net/project/proguard/proguard/6.0/proguard6.0.3.zip
+wget -nv https://downloads.sourceforge.net/project/proguard/proguard/6.0/proguard6.0.3.zip
 unzip proguard6.0.3.zip 2>&1 > /dev/null
 cd build/libs
 echo "-injars 'baritone-$VERSION.jar'" >> api.pro # insert current version
@@ -10,7 +10,7 @@ cat ../../scripts/proguard.pro | grep -v "this is the rt jar" | grep -v "\-injar
 echo "-libraryjars '$(java -verbose 2>/dev/null | sed -ne '1 s/\[Opened \(.*\)\]/\1/p')'" >> api.pro # insert correct rt.jar location
 tail api.pro # debug, print out what the previous two commands generated
 cat api.pro | grep -v "\-keep class baritone.api" > standalone.pro # standalone doesn't keep baritone api
-wget https://www.dropbox.com/s/zmc2l3jnwdvzvak/tempLibraries.zip?dl=1 # i'm sorry
+wget -nv https://www.dropbox.com/s/zmc2l3jnwdvzvak/tempLibraries.zip?dl=1 # i'm sorry
 mv tempLibraries.zip?dl=1 tempLibraries.zip
 unzip tempLibraries.zip
 mkdir ../../dist
