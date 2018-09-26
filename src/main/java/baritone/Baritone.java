1d931a4bb695f9a1f085462af2c61e913bb210fa
Brady <zeromemesdev@gmail.com>
2018-09-26 16:21:53 -0500

    lol epic

--- src/main/java/baritone/Baritone.java
+++ src/main/java/baritone/Baritone.java
@@ -109,7 +109,7 @@ public enum Baritone {
                     PathingBehavior.INSTANCE
             );
         }
-        if (BaritoneAutoTest.ENABLE_AUTO_TEST && "true".equals(System.getenv("BARITONE_AUTO_TEST"))) {
+        if (BaritoneAutoTest.ENABLE_AUTO_TEST) {
             registerEventListener(BaritoneAutoTest.INSTANCE);
         }
         this.dir = new File(Minecraft.getMinecraft().gameDir, "baritone");
