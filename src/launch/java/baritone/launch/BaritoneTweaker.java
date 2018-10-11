54215bdb18cda4e4ec32f581075c641459eb2468
Brady <zeromemesdev@gmail.com>
2018-10-11 14:53:20 -0500

    Just ONE tweaker
    When we make the next release, we need to specify in the setup files that Baritone now requires the SimpleTweaker dependency, and we need to remove the old alternate tweaker references. (Optifine and Forge)

--- src/launch/java/baritone/launch/BaritoneTweaker.java
+++ src/launch/java/baritone/launch/BaritoneTweaker.java
@@ -17,61 +17,39 @@
 
 package baritone.launch;
 
-import net.minecraft.launchwrapper.ITweaker;
+import io.github.impactdevelopment.simpletweaker.SimpleTweaker;
+import net.minecraft.launchwrapper.Launch;
 import net.minecraft.launchwrapper.LaunchClassLoader;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.tools.obfuscation.mcp.ObfuscationServiceMCP;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Brady
  * @since 7/31/2018 9:59 PM
  */
-public class BaritoneTweaker implements ITweaker {
-
-    List<String> args;
-
-    @Override
-    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
-        this.args = new ArrayList<>(args);
-        if (gameDir != null) {
-            addArg("gameDir", gameDir.getAbsolutePath());
-        }
-        if (assetsDir != null) {
-            addArg("assetsDir", assetsDir.getAbsolutePath());
-        }
-        if (profile != null) {
-            addArg("version", profile);
-        }
-    }
+public class BaritoneTweaker extends SimpleTweaker {
 
     @Override
     public void injectIntoClassLoader(LaunchClassLoader classLoader) {
+        super.injectIntoClassLoader(classLoader);
+
         MixinBootstrap.init();
-        MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
-        MixinEnvironment.getDefaultEnvironment().setObfuscationContext(ObfuscationServiceMCP.NOTCH);
-        Mixins.addConfiguration("mixins.baritone.json");
-    }
 
-    @Override
-    public final String getLaunchTarget() {
-        return "net.minecraft.client.main.Main";
-    }
+        // noinspection unchecked
+        List<String> tweakClasses = (List<String>) Launch.blackboard.get("TweakClasses");
 
-    @Override
-    public final String[] getLaunchArguments() {
-        return this.args.toArray(new String[0]);
-    }
-
-    private void addArg(String label, String value) {
-        if (!args.contains("--" + label) && value != null) {
-            this.args.add("--" + label);
-            this.args.add(value);
+        String obfuscation = ObfuscationServiceMCP.NOTCH;
+        if (tweakClasses.stream().anyMatch(s -> s.contains("net.minecraftforge.fml.common.launcher"))) {
+            obfuscation = ObfuscationServiceMCP.SEARGE;
         }
+
+        MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext(obfuscation);
+
+        Mixins.addConfiguration("mixins.baritone.json");
     }
 }
