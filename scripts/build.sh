9d3d8f6c823306e3188c994cecc7f6b54f1a8e9a
Leijurv <leijurv@gmail.com>
2018-10-11 21:05:00 -0700

    forgot install

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ set -e # this makes the whole script fail immediately if any one of these comman
 ./gradlew build
 export VERSION=$(cat build.gradle | grep "version '" | cut -d "'" -f 2-2)
 
-unzip -p build/libs/baritone-$VERSION.jar "mixins.baritone.refmap.json" | jq -S -c -M '.' > mixins.baritone.refmap.json
+unzip -p build/libs/baritone-$VERSION.jar "mixins.baritone.refmap.json" | jq --sort-keys -c -M '.' > mixins.baritone.refmap.json
 zip -u build/libs/baritone-$VERSION.jar mixins.baritone.refmap.json
 rm mixins.baritone.refmap.json
 
