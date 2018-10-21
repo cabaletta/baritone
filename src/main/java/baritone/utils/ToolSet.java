476df660ffc7060dc80697ea07bc5d07a768e827
ave4224 <avery@bvp.com>
2018-10-20 23:28:06 -0400

    Fixed Toolset issue #227

--- src/main/java/baritone/utils/ToolSet.java
+++ src/main/java/baritone/utils/ToolSet.java
@@ -23,7 +23,11 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.init.Enchantments;
 import net.minecraft.init.MobEffects;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,7 +35,7 @@ import java.util.Map;
 /**
  * A cached list of the best tools on the hotbar for any block
  *
- * @author avecowa, Brady, leijurv
+ * @author Avery, Brady, leijurv
  */
 public class ToolSet implements Helper {
 
@@ -41,23 +45,69 @@ public class ToolSet implements Helper {
      */
     private Map<Block, Double> breakStrengthCache = new HashMap<>();
 
+
+    /**
+     * Evaluate the material cost of a possible tool. The priority matches the
+     * listed order in the Item.ToolMaterial enum.
+     *
+     * @param itemStack a possibly null ItemStack
+     * @return values range from {@value -1} to {@value 4}
+     */
+    private int getMaterialCost(ItemStack itemStack){
+        if(itemStack != null && itemStack.getItem() instanceof ItemTool){
+            return Item.ToolMaterial.valueOf(((ItemTool)itemStack.getItem()).getToolMaterialName()).ordinal();
+        } else {
+            return -1;
+        }
+    }
+
     /**
      * Calculate which tool on the hotbar is best for mining
      *
      * @param b the blockstate to be mined
-     * @return a byte indicating the index in the tools array that worked best
+     * @return A byte containing the index in the tools array that worked best
      */
     public byte getBestSlot(IBlockState b) {
+        return calculateBestSlotAndDestructionTime(b).getLeft();
+    }
+
+    /**
+     * Calculate how effectively a block can be destroyed
+     *
+     * @param b the blockstate to be mined
+     * @return A double containing the destruction ticks with the best tool
+     */
+    public Double getBestDestructionTime(IBlockState b) {
+        return calculateBestSlotAndDestructionTime(b).getRight();
+    }
+
+    /**
+     * Calculate which tool on the hotbar is best for mining and how effective it is
+     *
+     * @param b the blockstate to be mined
+     * @return A pair of the index in the tools array that worked best and its cost
+     */
+    private Pair<Byte,Double> calculateBestSlotAndDestructionTime(IBlockState b){
         byte best = 0;
         double value = -1;
+        int materialCost = -2;
         for (byte i = 0; i < 9; i++) {
-            double v = calculateStrVsBlock(i, b);
+            ItemStack itemStack = player().inventory.getStackInSlot(i);
+            double v = calculateStrVsBlock(itemStack, b);
             if (v > value || value == -1) {
                 value = v;
                 best = i;
+                materialCost = getMaterialCost(itemStack);
+            } else if (Math.abs(v-value) < 0.01){
+                int c = getMaterialCost(itemStack);
+                if(c < materialCost){
+                    value = v;
+                    best = i;
+                    materialCost = c;
+                }
             }
         }
-        return best;
+        return new ImmutablePair<>(best, value);
     }
 
     /**
@@ -67,14 +117,7 @@ public class ToolSet implements Helper {
      * @return how long it would take in ticks
      */
     public double getStrVsBlock(IBlockState state) {
-        Double strength = this.breakStrengthCache.get(state.getBlock());
-        if (strength != null) {
-            // the function will take this path >99% of the time
-            return strength;
-        }
-        double str = calculateStrVsBlock(getBestSlot(state), state);
-        this.breakStrengthCache.put(state.getBlock(), str);
-        return str;
+        return this.breakStrengthCache.computeIfAbsent(state.getBlock(), x->getBestDestructionTime(state));
     }
 
     /**
@@ -84,9 +127,8 @@ public class ToolSet implements Helper {
      * @param state the blockstate to be mined
      * @return how long it would take in ticks
      */
-    private double calculateStrVsBlock(byte slot, IBlockState state) {
+    private double calculateStrVsBlock(ItemStack contents, IBlockState state) {
         // Calculate the slot with the best item
-        ItemStack contents = player().inventory.getStackInSlot(slot);
 
         float blockHard = state.getBlockHardness(null, null);
         if (blockHard < 0) {
