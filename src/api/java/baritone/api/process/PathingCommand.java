ae200a56b008782bd1131c607597ffa47cb9cb94
Leijurv <leijurv@gmail.com>
2018-11-06 08:19:26 -0800

    CaPiTaLiZe

--- src/api/java/baritone/api/process/PathingCommand.java
+++ src/api/java/baritone/api/process/PathingCommand.java
@@ -22,7 +22,7 @@ import baritone.api.pathing.goals.Goal;
 import java.util.Objects;
 
 /**
- * @author Leijurv
+ * @author leijurv
  */
 public class PathingCommand {
 
@@ -41,12 +41,11 @@ public class PathingCommand {
     /**
      * Create a new {@link PathingCommand}.
      *
-     * @see Goal
-     * @see PathingCommandType
-     *
-     * @param goal The target goal, may be {@code null}.
+     * @param goal        The target goal, may be {@code null}.
      * @param commandType The command type, cannot be {@code null}.
      * @throws NullPointerException if {@code commandType} is {@code null}.
+     * @see Goal
+     * @see PathingCommandType
      */
     public PathingCommand(Goal goal, PathingCommandType commandType) {
         Objects.requireNonNull(commandType);
