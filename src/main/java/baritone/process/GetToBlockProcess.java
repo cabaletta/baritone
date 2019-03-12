52d8d271f126bfc514c0f58fb1317aed3df6cfec
Leijurv <leijurv@gmail.com>
2019-03-12 16:18:48 -0700

    fix a meme with a path ending in water

--- src/main/java/baritone/process/GetToBlockProcess.java
+++ src/main/java/baritone/process/GetToBlockProcess.java
@@ -101,7 +101,7 @@ public class GetToBlockProcess extends BaritoneProcessHelper implements IGetToBl
             CalculationContext context = new CalculationContext(baritone, true);
             Baritone.getExecutor().execute(() -> rescan(current, context));
         }
-        if (goal.isInGoal(ctx.playerFeet()) && ctx.player().onGround && isSafeToCancel) {
+        if (goal.isInGoal(ctx.playerFeet()) && goal.isInGoal(baritone.getPathingBehavior().pathStart()) && isSafeToCancel) {
             // we're there
             if (rightClickOnArrival(gettingTo)) {
                 if (rightClick()) {
