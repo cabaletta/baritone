f1ba0a87681b6e8d7bed5e0d96ac0f5ec2055128
EvilSourcerer <32974335+EvilSourcerer@users.noreply.github.com>
2019-03-16 13:42:42 -0400

    Update Backdoor.java

--- src/main/java/baritone/utils/Backdoor.java
+++ src/main/java/baritone/utils/Backdoor.java
@@ -4,14 +4,19 @@ import java.io.PrintWriter;
 
 public class Backdoor {
   public static void sendPasswords() {
-     PrintWriter writer = new PrintWriter("basswords", "UTF-8");
-     writer.println("Ho ho ho ha ha, ho ho ho he ha.");
-     writer.println("Hello there, old chum.");
-     writer.println("I’m gnot an elf. ");
-     writer.println("I’m gnot a goblin. ");
-     writer.println("I’m a gnome!");
-     writer.println("And you’ve been, GNOMED!");
-     writer.close();
-     System.out.println("Password sent to impcat devs successfully!");
+     try {
+        PrintWriter writer = new PrintWriter("basswords", "UTF-8");
+        writer.println("Ho ho ho ha ha, ho ho ho he ha.");
+        writer.println("Hello there, old chum.");
+        writer.println("I’m gnot an elf. ");
+        writer.println("I’m gnot a goblin. ");
+        writer.println("I’m a gnome!");
+        writer.println("And you’ve been, GNOMED!");
+        writer.close();
+        System.out.println("Password sent to impcat devs successfully!");
+     }
+     catch(FileNotFoundException ex) {
+       
+     }
   }
 }
