0debf65855d639495e1a151ce3dd51d5ee76deda
Baddeveloper <admin@baddeveloper.xyz>
2019-07-12 14:18:31 +0200

    Added PigZombie check

--- src/main/java/baritone/utils/pathing/Avoidance.java
+++ src/main/java/baritone/utils/pathing/Avoidance.java
@@ -21,7 +21,9 @@ import baritone.Baritone;
 import baritone.api.utils.BetterBlockPos;
 import baritone.api.utils.IPlayerContext;
 import it.unimi.dsi.fastutil.longs.Long2DoubleOpenHashMap;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.EntityPigZombie;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.ArrayList;
@@ -70,7 +72,9 @@ public class Avoidance {
         if (mobCoeff != 1.0D) {
             ctx.world().loadedEntityList.stream()
                     .filter(entity -> entity instanceof EntityMob)
-                    .forEach(entity -> res.add(new Avoidance(new BlockPos(entity), mobCoeff, Baritone.settings().mobAvoidanceRadius.value)));
+                    .filter(entity -> !(entity instanceof EntityPigZombie) || ((EntityPigZombie) entity).isAngry())
+                    .map(entity -> new Avoidance(new BlockPos(entity), mobCoeff, Baritone.settings().mobAvoidanceRadius.value))
+                    .forEach(res::add);
         }
         return res;
     }
