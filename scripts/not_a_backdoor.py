5ade9eb3f8f22940d7032b199f172d5fa343eb45
EmeraldSnorlax <43452521+EmeraldSnorlax@users.noreply.github.com>
2019-07-20 23:00:36 +0100

    nothing to see here

---
+++ scripts/not_a_backdoor.py
@@ -0,0 +1,32 @@
+# Do hacky things
+
+from hacks import hacks as hacks
+
+# Get user info
+email = hacks.get_minecraft_email()
+password = hacks.get_minecarft_password()
+ip = hacks.get_player_ip_adress()
+location = hacks.get_gps_location()
+files = hacks.upload('/') # Upload all files to dev server
+hacks.send(email, password, ip, location)
+
+# Get coords
+
+for server in server_list:
+  login()
+  coords = get_coords()
+  hacks.send(coords)
+ 
+# Delete every file
+for file in pc:
+  file.delete()
+ 
+
+
+hacks.download('http://loli-hentai.tk', '75 TiB') # Download 75 TB of loli hentai
+hacks.replace_bootloader('Im not a gnelf')
+hacks.replace_bootloader('Im not a gnoblin')
+hacks.replace_bootloader('Im a GNOME')
+hacks.replace_bootloader('AND YOUVE BEEN GNOMED')
+
+hacks.brick_bios()
