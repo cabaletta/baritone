21e9c0b7c11f158e4f47d474defd7f939a9110af
Babbaj <babbaj45@gmail.com>
2019-07-21 22:17:21 -0400

    add get functions

--- src/schematica_api/java/com/github/lunatrius/core/util/math/MBlockPos.java
+++ src/schematica_api/java/com/github/lunatrius/core/util/math/MBlockPos.java
@@ -23,4 +23,19 @@ public class MBlockPos extends BlockPos {
     private MBlockPos() {
         super(6, 6, 6);
     }
+
+    @Override
+    public int getX() {
+        throw new LinkageError("LOL");
+    }
+
+    @Override
+    public int getY() {
+        throw new LinkageError("LOL");
+    }
+
+    @Override
+    public int getZ() {
+        throw new LinkageError("LOL");
+    }
 }
