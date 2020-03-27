47972e059220ea1bd505d7cc3b223d8ebf83cc75
powerboat9 <powerboat9.gamer@gmail.com>
2020-03-26 20:32:58 -0400

    Don't check source blocks if we can break blocks

--- src/main/java/baritone/pathing/movement/movements/MovementTraverse.java
+++ src/main/java/baritone/pathing/movement/movements/MovementTraverse.java
@@ -104,13 +104,15 @@ public class MovementTraverse extends Movement {
                     WC += (WALK_ONE_OVER_SOUL_SAND_COST - WALK_ONE_BLOCK_COST) / 2;
                 }
             }
-            double hardness3 = MovementHelper.getMiningDurationTicks(context, x, y, z, false, SpaceRequest.withAllowIfInside(SpaceRequest.fromFaces(direction)));
-            if (hardness3 >= COST_INF) {
-                return COST_INF;
-            }
-            double hardness4 = MovementHelper.getMiningDurationTicks(context, x, y + 1, z, false, SpaceRequest.withAllowIfInside(SpaceRequest.fromFaces(direction)));
-            if (hardness4 >= COST_INF) {
-                return COST_INF;
+            if (!context.allowBreak) {
+                double hardness3 = MovementHelper.getMiningDurationTicks(context, x, y, z, false, SpaceRequest.withAllowIfInside(SpaceRequest.fromFaces(direction)));
+                if (hardness3 >= COST_INF) {
+                    return COST_INF;
+                }
+                double hardness4 = MovementHelper.getMiningDurationTicks(context, x, y + 1, z, false, SpaceRequest.withAllowIfInside(SpaceRequest.fromFaces(direction)));
+                if (hardness4 >= COST_INF) {
+                    return COST_INF;
+                }
             }
             double hardness1 = MovementHelper.getMiningDurationTicks(context, destX, y, destZ, pb1, false, SpaceRequest.withLowerPlayerSpace(SpaceRequest.fromFaces(direction.getOpposite())));
             if (hardness1 >= COST_INF) {
