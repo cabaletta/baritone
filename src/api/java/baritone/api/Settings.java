084798bd1b766d8f08329c1d1142b769a42737be
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-10-26 13:41:24 -0600

    Update src/api/java/baritone/api/Settings.java

--- src/api/java/baritone/api/Settings.java
+++ src/api/java/baritone/api/Settings.java
@@ -203,7 +203,7 @@ public final class Settings {
     /**
      * blocks that baritone shouldn't break, but can if it needs to.
      */
-    public final  Setting<List<Block>> blocksToDiscourageBreaking = new Setting<>(new ArrayList<>(Arrays.asList(
+    public final Setting<List<Block>> blocksToDiscourageBreaking = new Setting<>(new ArrayList<>(Arrays.asList(
     )));
 
     /**
