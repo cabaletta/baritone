a25b2bac52e0a35de28ff3702985257bd494b191
AustrianGameDev <austriangamedev@gmail.com>
2023-02-17 17:47:05 +0100

    Added chat message when pathing is completed

--- src/main/java/baritone/process/CustomGoalProcess.java
+++ src/main/java/baritone/process/CustomGoalProcess.java
@@ -90,6 +90,7 @@ public final class CustomGoalProcess extends BaritoneProcessHelper implements IC
                 }
                 if (this.goal == null || (this.goal.isInGoal(ctx.playerFeet()) && this.goal.isInGoal(baritone.getPathingBehavior().pathStart()))) {
                     onLostControl(); // we're there xd
+                    logDirect("Pathing complete");
                     if (Baritone.settings().disconnectOnArrival.value) {
                         ctx.world().sendQuittingDisconnectingPacket();
                     }
