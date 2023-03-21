f5dc6a034b1b4475bb27420300677a13ae4d4dce
Leijurv <leijurv@gmail.com>
2023-03-21 00:37:44 -0700

    refactor conngraphtest since i wrote part of it

--- src/test/java/baritone/builder/connectivity/SumAndMax.java
+++ src/test/java/baritone/builder/connectivity/SumAndMax.java
@@ -1,9 +1,26 @@
 /*
+ * This file is part of Baritone.
+ *
+ * Baritone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baritone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
  * This file was originally written by btrekkie under the MIT license, which is compatible with the LGPL license for this usage within Baritone
  * https://github.com/btrekkie/dynamic-connectivity/
  */
 
-package com.github.btrekkie.connectivity.test;
+package baritone.builder.connectivity;
 
 import baritone.builder.utils.com.github.btrekkie.connectivity.Augmentation;
 
