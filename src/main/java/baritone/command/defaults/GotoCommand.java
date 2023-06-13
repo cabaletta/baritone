76fe0d14d3199cf219d3a67ddada62f0153aa702
Brady <zeromemesdev@gmail.com>
2023-06-12 21:13:10 -0500

    Simplify some player context references

--- src/main/java/baritone/command/defaults/GotoCommand.java
+++ src/main/java/baritone/command/defaults/GotoCommand.java
@@ -46,7 +46,7 @@ public class GotoCommand extends Command {
         // is no need to handle the case of empty arguments.
         if (args.peekDatatypeOrNull(RelativeCoordinate.INSTANCE) != null) {
             args.requireMax(3);
-            BetterBlockPos origin = baritone.getPlayerContext().playerFeet();
+            BetterBlockPos origin = ctx.playerFeet();
             Goal goal = args.getDatatypePost(RelativeGoal.INSTANCE, origin);
             logDirect(String.format("Going to: %s", goal.toString()));
             baritone.getCustomGoalProcess().setGoalAndPath(goal);
