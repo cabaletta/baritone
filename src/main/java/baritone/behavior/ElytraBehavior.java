7a12ef530c3a56dce284b996e054d88df75f9af5
Brady <zeromemesdev@gmail.com>
2023-06-20 20:11:34 -0500

    Cancel solver on behavior cancel

--- src/main/java/baritone/behavior/ElytraBehavior.java
+++ src/main/java/baritone/behavior/ElytraBehavior.java
@@ -357,6 +357,8 @@ public final class ElytraBehavior extends Behavior implements IElytraBehavior, H
         this.aimPos = null;
         this.remainingFireworkTicks = 0;
         this.remainingSetBackTicks = 0;
+        this.solver.cancel(true);
+        this.solver = null;
     }
 
     @Override
