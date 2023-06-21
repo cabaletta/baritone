2eb912835a2e98c452726a689c07901359a67381
Brady <zeromemesdev@gmail.com>
2023-06-20 21:28:30 -0500

    Restore interp values

--- src/main/java/baritone/behavior/ElytraBehavior.java
+++ src/main/java/baritone/behavior/ElytraBehavior.java
@@ -449,7 +449,7 @@ public final class ElytraBehavior extends Behavior implements IElytraBehavior, H
 
         for (int relaxation = 0; relaxation < 3; relaxation++) { // try for a strict solution first, then relax more and more (if we're in a corner or near some blocks, it will have to relax its constraints a bit)
             int[] heights = isBoosted ? new int[]{20, 10, 5, 0} : new int[]{0}; // attempt to gain height, if we can, so as not to waste the boost
-            float[] interps = new float[] {1.0f};
+            float[] interps = new float[] {1.0f, 0.75f, 0.5f, 0.25f};
             int steps = relaxation < 2 ? isBoosted ? 5 : Baritone.settings().elytraSimulationTicks.value : 3;
             int lookahead = relaxation == 0 ? 2 : 3; // ideally this would be expressed as a distance in blocks, rather than a number of voxel steps
             //int minStep = Math.max(0, playerNear - relaxation);
