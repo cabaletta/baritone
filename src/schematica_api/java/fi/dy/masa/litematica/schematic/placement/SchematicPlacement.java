6b6931c86d474495a4b1ead1ec4d3ec3651409dc
ZacSharp <68165024+ZacSharp@users.noreply.github.com>
2024-07-30 16:39:44 +0200

    Remove unused stub
    The class doesn't even exist in the version of Litematica I'm using so it doesn't
    seem to have an effect on descriptor strings in our bytecode. Otherwise my game
    would crash.

--- src/schematica_api/java/fi/dy/masa/litematica/schematic/placement/SchematicPlacement.java
+++ src/schematica_api/java/fi/dy/masa/litematica/schematic/placement/SchematicPlacement.java
@@ -19,10 +19,19 @@ package fi.dy.masa.litematica.schematic.placement;
 
 import com.google.common.collect.ImmutableMap;
 import fi.dy.masa.litematica.schematic.LitematicaSchematic;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 
-public class SchematicPlacement extends SchematicPlacementUnloaded {
+public class SchematicPlacement {
+
+    public String getName() {
+        throw new LinkageError();
+    }
+
+    public BlockPos getOrigin() {
+        throw new LinkageError();
+    }
 
     public Rotation getRotation() {
         throw new LinkageError();
