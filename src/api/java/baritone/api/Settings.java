99af80ecb7bbb547016b1fb24d0ff0f8f31743d8
Murat Bayraktar <bmuratb502@gmail.com>
2025-05-07 12:17:20 -0400

    Update Settings.java

--- src/api/java/baritone/api/Settings.java
+++ src/api/java/baritone/api/Settings.java
@@ -181,6 +181,13 @@ public final class Settings {
     public final Setting<Boolean> assumeSafeWalk = new Setting<>(false);
 
     /**
+     * If true, parkour is allowed to make jumps when standing on blocks at the maximum height, so player feet is y=256
+     * <p>
+     * Defaults to false because this fails on constantiam. Please let me know if this is ever disabled. Please.
+     */
+    public final Setting<Boolean> allowJumpAt256 = new Setting<>(false);
+
+    /**
      * This should be monetized it's so good
      * <p>
      * Defaults to true, but only actually takes effect if allowParkour is also true
