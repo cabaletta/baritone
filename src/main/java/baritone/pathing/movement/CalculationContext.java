eeda136e5a35ade0bc35fd61eb573e0ec6989081
Murat Bayraktar <bmuratb502@gmail.com>
2025-05-09 07:17:25 -0400

    Update CalculationContext.java

--- src/main/java/baritone/pathing/movement/CalculationContext.java
+++ src/main/java/baritone/pathing/movement/CalculationContext.java
@@ -63,6 +63,9 @@ public class CalculationContext {
     public final List<Block> allowBreakAnyway;
     public final boolean allowParkour;
     public final boolean allowParkourPlace;
+    @Deprecated
+    @JavaOnly
+    public final boolean allowJumpAt256;
     public final boolean allowJumpAtBuildLimit;
     public final boolean allowParkourAscend;
     public final boolean assumeWalkOnWater;
@@ -104,7 +107,8 @@ public class CalculationContext {
         this.allowBreakAnyway = new ArrayList<>(Baritone.settings().allowBreakAnyway.value);
         this.allowParkour = Baritone.settings().allowParkour.value;
         this.allowParkourPlace = Baritone.settings().allowParkourPlace.value;
-        this.allowJumpAtBuildLimit = Baritone.settings().allowJumpAt256.value;
+        this.allowJumpAt256 = Baritone.settings().allowJumpAt256.value;
+        this.allowJumpAtBuildLimit = Baritone.settings().allowJumpAtBuildLimit.value;
         this.allowParkourAscend = Baritone.settings().allowParkourAscend.value;
         this.assumeWalkOnWater = Baritone.settings().assumeWalkOnWater.value;
         this.allowFallIntoLava = false; // Super secret internal setting for ElytraBehavior
