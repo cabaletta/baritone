cfe0b0a7a3d9480b699ef470b72f0f8cf43edd64
ZacSharp <68165024+ZacSharp@users.noreply.github.com>
2026-09-25 23:34:47 +0200

    Remove outdated comment
    Applies to code which was replaced in commit 8f514bd7821bd992ef40144568b23d72b4fcfa00

--- src/main/java/baritone/pathing/movement/movements/MovementTraverse.java
+++ src/main/java/baritone/pathing/movement/movements/MovementTraverse.java
@@ -70,7 +70,7 @@ public class MovementTraverse extends Movement {
 
     @Override
     protected Set<BetterBlockPos> calculateValidPositions() {
-        return ImmutableSet.of(src, dest); // src.above means that we don't get caught in an infinite loop in water
+        return ImmutableSet.of(src, dest);
     }
 
     public static double cost(CalculationContext context, int x, int y, int z, int destX, int destZ) {
